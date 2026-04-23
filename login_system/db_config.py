8e50384b4f4b456e32c3a86b47cbe2f41c1248a0
nage143 <tnageswarao088@gmail.com>
2026-04-23 10:13:49 +0530

    Add files via upload

---
+++ login_system/db_config.py
@@ -0,0 +1,10 @@
+import mysql.connector
+
+def get_db_connection():
+    return mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="Rao@1234",
+        database="employee_portal"
+        
+    )
